f754bde5d1e615347d1e24fb30e1b369150d2d33
Peter Jihoon Kim <raingrove@gmail.com>
2017-05-29 01:25:02 -0700

    Display web view

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,37 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View
-} from 'react-native';
+} from 'react-native'
+import WKWebView from 'react-native-wkwebview-reborn'
 
 export default class Nabi extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <WKWebView
+          style={styles.webView}
+          source={{uri: 'http://example.com/'}}
+        />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'stretch',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  webView: {
+    flex: 1
+  }
+})
 
-AppRegistry.registerComponent('Nabi', () => Nabi);
+AppRegistry.registerComponent('Nabi', () => Nabi)
