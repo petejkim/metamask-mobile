6c5551bb3d7bcdface084acc6b6fe052e9693596
Peter Jihoon Kim <raingrove@gmail.com>
2017-06-04 03:16:59 -0700

    Reinitialize iOS project with Swift

---
+++ ios/Nabi/Nabi-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "NSURLProtocol+WKWebViewSupport.h"
+#import "AppProtocol.h"
+
+#import <React/RCTBundleURLProvider.h>
+#import "RCCManager.h"
